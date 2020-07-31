556b5377293cac63ff8e01310fd70adf263455a1
Varun <31084854+Borumer@users.noreply.github.com>
2020-07-31 14:04:04 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# undergroundrailroad_bytehacks
+ 
---
+++ android/app/src/main/kotlin/com/example/undergroundrailroadbytehacks/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.undergroundrailroadbytehacks
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
